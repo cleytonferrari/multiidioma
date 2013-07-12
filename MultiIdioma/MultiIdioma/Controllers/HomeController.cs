fdbef6df242dcd15591e304d0d5e1dce157c5950
Cleyton Ferrari <cleyton@w7br.com>
2013-07-12 17:28:28 -0400

    projeto inicial

---
+++ MultiIdioma/MultiIdioma/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiIdioma.Helper;
+
+namespace MultiIdioma.Controllers
+{
+    public class HomeController : Controller
+    {
+        //para testar 
+        // http://localhost:XXXX/en-US
+        // http://localhost:XXXX/pt-BR
+
+
+        [Localization]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MultiIdioma/MultiIdioma/Helper/LocalizedRazorViewEngine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiIdioma.Helper
+{
+    public class LocalizedRazorViewEngine : RazorViewEngine
+    {
+        public LocalizedRazorViewEngine()
+        {
+            DefaultLanguageCode = "pt-BR";
+        }
+        public string DefaultLanguageCode { get; set; }
+
+        public override ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            var language = DefaultLanguageCode;
+
+            var controllerName = (string)controllerContext.RouteData.Values["controller"];
+
+            if (controllerContext.RouteData.Values["lang"] != null 
+                && !string.IsNullOrWhiteSpace(controllerContext.RouteData.Values["lang"].ToString()))
+                language = controllerContext.RouteData.Values["lang"].ToString();
+
+            if (language != "") language = "." + language;
+
+            var masterPath = string.Format("~/Views/Shared/_Layout{0}.cshtml", language);
+            var uri = string.Format("~/Views/{0}/{1}{2}.cshtml", controllerName, viewName, language);
+
+            if (VirtualPathProvider.FileExists(uri))
+                return new ViewEngineResult(CreateView(controllerContext, uri, masterPath), this);
+
+            return base.FindView(controllerContext, viewName, masterName, useCache);
+        }
+
+    }
+}
