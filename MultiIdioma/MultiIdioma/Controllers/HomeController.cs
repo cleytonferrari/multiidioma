2a65dcec167480303c83a9dada560c3e30f2e8c3
Cleyton Ferrari <cleyton@w7br.com>
2013-07-31 15:37:45 -0400

    adicionado o attribute route, e criado algumas views

--- MultiIdioma/MultiIdioma/Controllers/HomeController.cs
+++ MultiIdioma/MultiIdioma/Controllers/HomeController.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AttributeRouting.Web.Mvc;
 using MultiIdioma.Helper;
 
 namespace MultiIdioma.Controllers
 {
     public class HomeController : Controller
     {
-        //para testar 
-        // http://localhost:XXXX/en-US
-        // http://localhost:XXXX/pt-BR
-
-
         [Localization]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [GET("{lang?}/Home/About")]
+        [GET("{lang?}/Home/Sobre")]
+        public ActionResult Sobre()
         {
             return View();
         }
